701836cf16e099432c6aab7027d78af6a14877cf
Burak Senturk <senturkburk@gmail.com>
2023-02-03 15:58:22 +0300

    Rezervasyon sayfası düzenlendi

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,19 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -18,12 +26,23 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            //destination bilgisini dropdown dan alıcaz
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString() // kullanmıcaz ama..
+                                           }).ToList();
+            ViewBag.v=values;
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 2;  //şimdilik static yaptım
+            p.Status = "Onay bekliyor.";
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");  //aktif rezervasyonlarıma yönlendirecek
         }
     }
 }
