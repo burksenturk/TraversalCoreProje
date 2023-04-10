bffcb4e223c3700b18b94ecf90f5df7d26d556b7
Burak Senturk <senturkburk@gmail.com>
2023-04-10 22:57:44 +0300

    Destination ve Reservation ilişkisi kuruldu

--- EntityLayer/Concrete/Reservation.cs
+++ EntityLayer/Concrete/Reservation.cs
@@ -12,9 +12,10 @@ namespace EntityLayer.Concrete
         public int AppUserId { get; set; }
         public AppUser AppUser { get; set; }  //ad soyadları appuser dan çekicem
         public string PersonCount { get; set; }
-        public string Destination { get; set; }
         public DateTime ReservationDate { get; set; }
         public string Desctiption { get; set; }
         public string Status { get; set; }  //bool yapmadık.. kendi içinde rez onaylandı onaylanmadı , müşteriye teklif verielcek  vs.. gibi filtreleme işlemine tabii tutucam.
+        public int DestinationID { get; set; }
+        public Destination Destination { get; set; }
     }
 }
