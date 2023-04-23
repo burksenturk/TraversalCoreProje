4a006ae3f28adecb4c9545f4d6837549e26c253c
Burak Senturk <senturkburk@gmail.com>
2023-04-23 15:17:17 +0300

    Ajax işlemleri gerçekleştirildi. Destination tablosu ile çalışıldı.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller   //Ajax işlemleri için olusturdum 
+    {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
+            return Json(jsonCity);
+        }
+        public IActionResult GetById(int DestinationID) 
+        {
+            var value = _destinationService.TGetByID(DestinationID);
+            var jsonvalue = JsonConvert.SerializeObject(value);
+            return Json(jsonvalue);
+        }
+
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
+        {
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+
+        public IActionResult DeleteCity(int id)
+        {
+            var values = _destinationService.TGetByID(id);
+            _destinationService.TDelete(values);
+            return NoContent();
+        }
+
+        public IActionResult UpdateCity(Destination destination)
+        {
+            _destinationService.TUpdate(destination);
+            var v = JsonConvert.SerializeObject(destination);
+            return Json(v);
+        }
+    }
+}
---
+++ TraversalCoreProje/Models/Cityclass.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class Cityclass
+    {
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
