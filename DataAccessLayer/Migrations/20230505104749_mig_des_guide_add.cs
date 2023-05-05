f4336975123db42312acb08b3f7d2d98849de7a0
Burak Senturk <senturkburk@gmail.com>
2023-05-05 18:04:50 +0300

    Tasarımsal düzenlemeler. yapıldı UI tarafında sayfalar tamamlandı

---
+++ DataAccessLayer/Migrations/20230505104749_mig_des_guide_add.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_des_guide_add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description2",
+                table: "Guides",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GuideListImage",
+                table: "Guides",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Destinations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description2",
+                table: "Guides");
+
+            migrationBuilder.DropColumn(
+                name: "GuideListImage",
+                table: "Guides");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Destinations");
+        }
+    }
+}
